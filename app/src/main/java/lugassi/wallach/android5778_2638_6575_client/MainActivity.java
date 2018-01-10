3353baacd0e53d57182d316b86902fd5de87187c
mlugassi <michael19191@gmail.com>
2018-01-10 17:21:04 +0200

    Initial commit

---
+++ app/src/main/java/lugassi/wallach/android5778_2638_6575_client/MainActivity.java
@@ -0,0 +1,13 @@
+package lugassi.wallach.android5778_2638_6575_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+    }
+}
